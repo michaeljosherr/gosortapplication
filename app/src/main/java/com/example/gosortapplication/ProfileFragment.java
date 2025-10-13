49eac154c7084311f78c4ebbd35652ece74c5b86
megel <132318881+Rymdsylt@users.noreply.github.com>
2025-10-13 22:02:10 +0800

    fix'd

--- app/src/main/java/com/example/gosortapplication/ProfileFragment.java
+++ app/src/main/java/com/example/gosortapplication/ProfileFragment.java
@@ -1,8 +1,6 @@
 package com.example.gosortapplication;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,7 +9,6 @@ import android.widget.ImageView;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
